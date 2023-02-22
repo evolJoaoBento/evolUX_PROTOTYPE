0a56442b0717c0bdfd89685de9041f85245997f9
JoaoBento <joao.bento@evolsolutions.pt>
2023-02-22 19:36:32 +0000

    Production Report Print styling almost final

--- evolUX.UI/wwwroot/js/site.js
+++ evolUX.UI/wwwroot/js/site.js
@@ -215,3 +215,20 @@ printerOpt.addEventListener('click', function () {
         printerOpt.parentElement.classList.add('print-filter-open');
     }
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.height) {
+            content.style.height = null;
+            content.style.opacity = "0";
+        } else {
+            content.style.height = content.scrollHeight + 20 + "px";
+            content.style.opacity = "100%";
+        } 
+    });
+}
