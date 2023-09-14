7fdb65c7689da9fb02ba41ba99631e8158de2b10
ZivileSnk <sink.zivile@gmail.com>
2023-09-14 12:58:46 +0100

    9/14

--- Shared/Models/Areas/Reports/DependentEnvsInfo.cs
+++ Shared/Models/Areas/Reports/DependentEnvsInfo.cs
@@ -2,14 +2,14 @@
 {
     internal class DependentEnvsInfo
     {
-        public int MyProperty { get; set; } //Data/Hora da impressão
-        public int MyProperty { get; set; } //Nome da área de negócio
-        public int MyProperty { get; set; } //Tipo de expedição
-        public int MyProperty { get; set; } //Companhia de expedição/tratamento
-        public int MyProperty { get; set; } //Formato de envelope
+        public int EndTimeStamp { get; set; } //Data/Hora da impressão
+        public int Description { get; set; } //Nome da área de negócio
+        public int ExpeditionType { get; set; } //Tipo de expedição
+        public int ExpCode { get; set; } //Companhia de expedição/tratamento
+        public int FullFillMaterialCode { get; set; } //Formato de envelope
         public int MyProperty { get; set; } //Tipo de envelopagem
-        public int MyProperty { get; set; } //Ficheiros
-        public int MyProperty { get; set; } //Docs
-        public int MyProperty { get; set; } //Envelopagens
+        public int FileID { get; set; } //Ficheiros
+        public int TotalDocs { get; set; } //Docs
+        public int TotalPostObjs { get; set; } //Envelopagens
     }
 }
--- Shared/Models/Areas/Reports/DependentPrintsInfo.cs
+++ Shared/Models/Areas/Reports/DependentPrintsInfo.cs
@@ -2,13 +2,13 @@
 {
     internal class DependentPrintsInfo
     {
-        public int MyProperty { get; set; } //Data/Hora da impressão
-        public int MyProperty { get; set; } //Nome da área de negócio
-        public int MyProperty { get; set; } //Tipo de expedição
-        public int MyProperty { get; set; } //Companhia de expedição/tratamento
-        public int MyProperty { get; set; } //Tipo de impressão
-        public int MyProperty { get; set; } //Ficheiros
-        public int MyProperty { get; set; } //Docs
-        public int MyProperty { get; set; } //Impressões
+        public int EndTimeStamp { get; set; } //Data/Hora da impressão
+        public int Description { get; set; } //Nome da área de negócio
+        public int ExpeditionType { get; set; } //Tipo de expedição
+        public int ExpCode { get; set; } //Companhia de expedição/tratamento
+        public int PlexCode { get; set; } //Tipo de impressão
+        public int FileID { get; set; } //Ficheiros
+        public int TotalDocs { get; set; } //Docs
+        public int TotalPrint { get; set; } //Impressões
     }
 }
